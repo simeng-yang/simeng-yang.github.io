cac15412c05934bb4106bfece4886a5840bc361c
simeng-yang <yangsimeng@yahoo.ca>
2017-02-24 10:26:09 -0500

    New

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "http://www.simengyang.me/mail/contact_me.php",
+                url: "contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
